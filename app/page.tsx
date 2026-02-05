79206ee513b85a71a2c462b1a5bbefd1cfc14279
tsakib2000 <isakib49@gmail.com>
2026-02-06 03:22:21 +0600

    added background

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
-import Image from "next/image";
+import { NavMain } from "@/components/nav-main";
 import React from "react";
 
 export default function Home(): React.ReactElement {
   return (
-    <div className="flex min-h-screen items-center justify-center  font-sans ">
-<h1 className='text-red-500'>Welcome to home page
-</h1>
+    <div className="flex min-h-screen items-center justify-center bg-black font-sans ">
+       <div 
+    className="absolute inset-0 opacity-50"
+    style={{
+      backgroundImage: ' radial-gradient(circle, rgb(151 151 151) 1px, transparent 1px)',
+      backgroundSize: '30px 30px'
+    }}
+  />
+
     </div>
   );
 };
