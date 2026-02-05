a7ab993c012aa4f727f1554a6e4a780f847ca357
Tauhidul Islam <tauhidulislam@Tauhiduls-MacBook-Air.local>
2026-02-05 12:54:35 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Home(): React.ReactElement {
+  return (
+    <div className="flex min-h-screen items-center justify-center  font-sans ">
+<h1 className='text-red-500'>Welcome to home page
+</h1>
+    </div>
+  );
+};
