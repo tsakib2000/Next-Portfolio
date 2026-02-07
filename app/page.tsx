fb91e9248f52d273deaec569423aa0bbf3fc3dbc
tsakib2000 <isakib49@gmail.com>
2026-02-08 04:11:07 +0600

    changed background

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
-import { NavMain } from "@/components/nav-main";
+import { Skills } from "@/components/skills";
+import { FlickeringGrid } from "@/components/ui/flickering-grid";
 import React from "react";
 
 export default function Home(): React.ReactElement {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-black font-sans ">
-       <div 
-    className="absolute inset-0 opacity-50"
-    style={{
-      backgroundImage: ' radial-gradient(circle, rgb(151 151 151) 1px, transparent 1px)',
-      backgroundSize: '30px 30px'
-    }}
-  />
+  return (<div className="bg-black relative min-h-screen w-full overflow-hidden ">
+           <FlickeringGrid
+        className="absolute inset-0 z-0 size-full"
+        squareSize={4}
+        gridGap={6}
+        color="#6B7280"
+        maxOpacity={0.5}
+        flickerChance={0.1} 
+      />
+      {/* <Skills/> */}
 
-    </div>
+      </div>
   );
 };
