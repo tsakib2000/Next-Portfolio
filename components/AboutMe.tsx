d9ee685badeeaece25b3bbbf72b5cff0b1b85ae4
tsakib2000 <isakib49@gmail.com>
2026-02-10 00:00:11 +0600

    added typewritter effect

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -2,6 +2,7 @@
 import { useTheme } from 'next-themes';
 import Image from 'next/image'
 import { LineShadowText } from './ui/line-shadow-text';
+import Typewriter from 'typewriter-effect';
 
 
 function AboutMe() {
@@ -11,17 +12,25 @@ function AboutMe() {
         <div className=' h-auto flex justify-between items-center z-10'>
             <div>
                 <h1 className="text-3xl leading-none font-semibold tracking-tighter text-balance sm:text-6xl md:text-7xl lg:text-8xl z-50">
-                Hi, I'm 
-                <LineShadowText className="italic" shadowColor={shadowColor}>
-                   Tauhidul 
-                </LineShadowText></h1>
-                <p>Forntend Developer</p>
+                    Hi, I'm
+                    <LineShadowText className="italic" shadowColor={shadowColor}>
+                        Tauhidul
+                    </LineShadowText></h1>
+                <h1 className='text-3xl mt-3.5'>
+                    <Typewriter
+                    options={{
+                        strings: ['Frontend Developer', 'Mern Stack developer'],
+                        autoStart: true,
+                        loop: true,
+                    }}
+                />
+                </h1>
             </div>
             <div className='z-30'>
                 <Image
                     src="/profile.png"
-                    width={200}
-                    height={200}
+                    width={300}
+                    height={300}
                     alt="Picture of the author"
                 />
             </div>
